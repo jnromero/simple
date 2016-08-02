d0ce468e5ee080afaacc4128bca7468a66f91fc3
jnromero <jnromero@gmail.com>
2016-08-01 22:28:49 -0700

    Minor changes to steep
    Minor changes to steep

--- files/config.py
+++ files/config.py
@@ -6,59 +6,31 @@ scriptPath=os.path.dirname(os.path.realpath(__file__))
 
 def defaultSettings(location,configFile,serverStartString):
 	config={}
-	config['favicon']="common/"
 
-	config['packageFolder']="/common/"
+	config['packageFolder']="/steep/"
 	config['currentExperiment']="/simple/"
-	# config['instructionsFolder']="/files/instructions/"
-	# config['instructionsTexFile']="/latex/instructions.tex"
-	# config['quiz']=True
-	config["location"]=location
-	config['screenServerPort']=15374
+	config['screenServerPort']=1234
 
-	if location=="local":
-		config['webServerRoot']="/Users/jnr/Dropbox/Sites/jnromero.com/Experiments/"
+	if location=="myComputer":
+		config['webServerRoot']="/Users/myUsername/experiments/"
 		config['serverType']="regularExperiment"	
-		config['serverPort']=24921
-		config['webSocketPort']=13557
+		config['serverPort']=2345
+		config['webSocketPort']=3456
 		ip="localhost"
 		config["domain"]="http://"+ip+":"+str(config['serverPort'])
 		config["websocketURL"]="ws://"+ip+":"+str(config['webSocketPort'])
 		config["screenServer"]="http://"+ip+":"+str(config['screenServerPort'])
-	# elif location=="esl":
-	# 	config['webServerRoot']="/Users/jnr/Dropbox/Sites/jnromero.com/Experiments/"
+	# elif location=="labComputer":
+	# 	config['webServerRoot']="/Users/labAdmin/myUsername/experiments/"
 	# 	config['serverType']="regularExperiment"	
-	# 	config['serverPort']=24921
-	# 	config['webSocketPort']=13557
-	# 	ip="10.128.228.70"
+	# 	config['serverPort']=4567
+	# 	config['webSocketPort']=5678
+	# 	ip="12.345.67.89"
 	# 	config["domain"]="http://"+ip+":"+str(config['serverPort'])
 	# 	config["websocketURL"]="ws://"+ip+":"+str(config['webSocketPort'])
 	# 	config["screenServer"]="http://"+ip+":"+str(config['screenServerPort'])
-	# elif location=="webf":
-	# 	config['webServerRoot']="/home/jnromero/Sites/jnromero.com/Experiments/"
-	# 	config['serverType']="regularExperiment"	
-	# 	config['serverPort']=24921
-	# 	config['webSocketPort']=13557
-	# 	config["domain"]="http://jnromero.com/experiment"
-	# 	config["websocketURL"]='ws://jnromero.com/webSockets/mainExperiment'
-	# 	config["screenServer"]="http://jnromero.com/screenServer/"
-	# elif location=="localDemo":
-	# 	config['webServerRoot']="/Users/jnr/Dropbox/Sites/jnromero.com/Experiments/"
-	# 	config['serverType']="demoExperiment"	
-	# 	config['serverPort']=18268
-	# 	config['webSocketPort']=13237
-	# 	ip="localhost"
-	# 	config["domain"]="http://"+ip+":"+str(config['serverPort'])
-	# 	config["websocketURL"]="ws://"+ip+":"+str(config['webSocketPort'])
-	# 	config["screenServer"]="http://"+ip+":"+str(config['screenServerPort'])
-	# elif location=="webfDemo":
-	# 	config['webServerRoot']="/home/jnromero/Sites/jnromero.com/Experiments/"
-	# 	config['serverType']="demoExperiment"	
-	# 	config['serverPort']=18268
-	# 	config['webSocketPort']=13237
-	# 	config["domain"]="http://jnromero.com/experiments/demos/normalForm"
-	# 	config["websocketURL"]='ws://jnromero.com/webSockets/demos/normalForm'
-	# 	config["screenServer"]="http://jnromero.com/screenServer/"
+
+	config["location"]=location
 
 	config=setOtherFileLocations(config,serverStartString)
 	writeJavascriptConfigFile(config,configFile)

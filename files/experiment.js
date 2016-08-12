d2f78201b01889235e0a6b4a2393f6913ad956d3
jnromero <jnromero@gmail.com>
2016-08-11 22:13:46 -0700

    Added variable declarations
    Added some variable declarations to clean things up.  Also added in the
    new “clickButton” function.

--- files/experiment.js
+++ files/experiment.js
@@ -3,15 +3,14 @@
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
 
 function drawButton() {
-    makeChoiceButton = createDiv("makeChoiceButton");
-    var pf = partial(makeChoiceButtonClicked, 27);
-    makeChoiceButton.addEventListener("click", pf);
+    var makeChoiceButton = createDiv("makeChoiceButton");
     makeChoiceButton.innerHTML = "Click Here to Submit";
     $("#mainDiv").append(makeChoiceButton);
+    clickButton("many","makeChoiceButton",makeChoiceButtonClicked,27);
 }
 
 function drawMatch(currentMatch) {
-    matchText = createDiv("matchText");
+    var matchText = createDiv("matchText");
     matchText.innerHTML = "Match #" + currentMatch + "  Time: <time id='timer'>0</time>" + "  Self timer: <time id='selfTimer'>0</time>";
     moveTimer("selfTimer");
     moveTimer("timer");
@@ -19,13 +18,13 @@ function drawMatch(currentMatch) {
 }
 
 function drawStatus(number) {
-    numberClicks = createDiv("numberClicks");
+    var numberClicks = createDiv("numberClicks");
     numberClicks.innerHTML = "There have been " + number + " clicks so far.";
     $("#mainDiv").append(numberClicks);
 }
 
 function pleaseMakeChoice(message) {
-    matchText = document.getElementById("matchText");
+    var matchText = document.getElementById("matchText");
     matchText.style.color = "red";
     matchText.style.fontSize = "200%";
 }
@@ -63,10 +62,10 @@ function reconnecting(msg) {
 
 
 function statusManager() {
-    thisStatus = window.state;
+    var thisStatus = window.state;
     console.log(thisStatus)
     if (thisStatus[0] == -1) {
-        message = "Loading...";
+        var message = "Loading...";
         genericScreen(message);
     } else if (thisStatus["page"] == "generic") {
         clearAll();
